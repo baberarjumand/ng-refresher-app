0af5921b6b318dd2d403427a0e5e8de7e325808c
Baber Arjumand <10764414+baberarjumand@users.noreply.github.com>
2024-10-24 21:32:08 +0500

    init basic routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { PageAComponent } from './pages/page-a/page-a.component';
+import { PageBComponent } from './pages/page-b/page-b.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'page-a',
+    component: PageAComponent,
+  },
+  {
+    path: 'page-b',
+    component: PageBComponent,
+  },
+  {
+    path: 'home',
+    component: HomeComponent,    
+  },
+  {
+    path: '**',
+    redirectTo: '/home',
+  },
+];
